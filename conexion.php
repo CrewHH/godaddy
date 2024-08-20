865faec6eadd0291ea6d3059c180a9d360918efd
CrewHH <136742131+CrewHH@users.noreply.github.com>
2024-08-19 20:50:28 -0500

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,19 @@
+<?php
+// Configura los detalles de conexión a la base de datos
+$servername = "localhost";
+$username = "cures_dev";
+$password = "yZJSUpXg4tnu";
+$dbname = "arsocial_db";
+
+// Crea la conexión
+$conexion = new mysqli($servername, $username, $password, $dbname);
+
+// Verifica la conexión
+if ($conexion->connect_error) {
+    die("Conexión fallida: " . $conexion->connect_error);
+}
+
+//
+//cures_dev
+//yZJSUpXg4tnu
+//arsocial_db
